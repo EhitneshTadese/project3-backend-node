e2319e47d3fae48c85323f5e047cd4440f45d7ff
ehitnesh.k.tadese@eagles.oc.edu <ehitekibret.com>
2024-11-06 03:39:32 -0600

    fixed error in index.js file in models

--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,23 @@
 const dbConfig = require("../config/db.config.js");
+
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  operatorsAliases: false,
+
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+    idle: dbConfig.pool.idle,
+  },
 });
+
 const db = {};
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-<<<<<<< HEAD
 
 // Use consistent naming, "resume" singular form
 //db.resume = require("./user.model.js")(sequelize, Sequelize);
@@ -28,7 +31,3 @@ db.sequelize = sequelize;
 module.exports = db;
 
 
-=======
-db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
-module.exports = db;
->>>>>>> 8598cb4702ec7e44e0e4932ef62bf4c45c408379
